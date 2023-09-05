d58b10bca91a18e22fd9b06d7ab57ebd70b1e47f
Revaz-Goguadze <goguadze.rezi2022@gmail.com>
2023-09-05 17:00:44 +0400

    aeae

--- JsonSerialization/Company.cs
+++ JsonSerialization/Company.cs
@@ -4,16 +4,30 @@ namespace JsonSerialization
 {
     public class Company
     {
+        [JsonPropertyName("company_name")]
+        [JsonPropertyOrder(1)]
         public string? Name { get; set; }
 
+        [JsonPropertyName("company_established_year")]
+        [JsonPropertyOrder(2)]
         public int EstablishedYear { get; set; }
 
+        [JsonPropertyName("company_employee_count")]
+        [JsonPropertyOrder(3)]
         public int EmployeeCount { get; set; }
 
+        [JsonPropertyName("company_domains")]
+        [JsonPropertyOrder(4)]
+#pragma warning disable CA2227
         public Dictionary<string, int>? Domains { get; set; }
+#pragma warning restore CA2227
 
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        [JsonPropertyName("company_company_type")]
+        [JsonPropertyOrder(5)]
         public CompanyType CompanyType { get; set; }
 
+        [JsonIgnore]
         public string? IgnoreProperty { get; set; }
     }
 }
--- JsonSerialization/JsonSerializationOperations.cs
+++ JsonSerialization/JsonSerializationOperations.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -9,38 +10,66 @@ namespace JsonSerialization
     {
         public static string SerializeObjectToJson(object obj)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            // Serialize the provided object to JSON.
+            return JsonSerializer.Serialize(obj);
         }
 
         public static T? DeserializeJsonToObject<T>(string json)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            // Deserialize the JSON string to an object of type T.
+            return JsonSerializer.Deserialize<T>(json);
         }
 
         public static string SerializeCompanyObjectToJson(object obj)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            // Serialize the provided object to JSON with custom options.
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                Converters = { new JsonStringEnumConverter() },
+            };
+            return JsonSerializer.Serialize(obj, options);
         }
 
         public static T? DeserializeCompanyJsonToObject<T>(string json)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            // Deserialize the JSON string to an object of type T with custom options.
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                Converters = { new JsonStringEnumConverter() },
+            };
+            return JsonSerializer.Deserialize<T>(json, options);
         }
 
         public static string SerializeDictionary(Company obj)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase, // Serialize keys in camelCase
+            };
+
+            // Create a new dictionary with modified keys (lowercase first letter)
+            var modifiedDictionary = new Dictionary<string, int>();
+#pragma warning disable CA1062
+            foreach (var kvp in obj.Domains!)
+#pragma warning restore CA1062
+            {
+#pragma warning disable CA1304
+                var modifiedKey = char.ToLower(kvp.Key[0]) + kvp.Key.Substring(1);
+#pragma warning restore CA1304
+                modifiedDictionary[modifiedKey] = kvp.Value;
+            }
+
+            return JsonSerializer.Serialize(modifiedDictionary, options);
         }
 
         public static string SerializeEnum(Company obj)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            // Serialize the CompanyType enum property to JSON.
+#pragma warning disable CA1062
+            return JsonSerializer.Serialize(obj.CompanyType);
+#pragma warning restore CA1062
         }
     }
 }
